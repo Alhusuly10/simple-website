9d5f216632589f5126b6a2fde55c2db5b5667b8b
Alhusly10 <sulaimanabdulmuheez@gmail.com>
2022-10-24 00:37:11 +0100

    projects files

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+const firstName = "Abdulmuiz";
+const aboutMe = [
+    firstName,
+    2022-2001,
+]
+
+aboutMe[2] = "I am a programmer";
+console.log(aboutMe);
+ 
+aboutMe.push("freeCodeCamp");
+aboutMe.unshift("sulaiman");
+console.log(aboutMe[aboutMe.length - 2])
+
+const newObject = { 
+    "first Name" : "Abdul muheez",
+    profession: "programming",
+    school: "University of Ibadan",
+
+}
+
+console.log(newObject["profession"])
+
+newObject["first Name"] = "sulaiman"
+console.log(newObject)
+
+
+
